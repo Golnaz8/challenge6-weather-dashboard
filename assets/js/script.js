e91c986759fb8cd2b992bb8cfddcc8895d8b37c4
Golnaz8 <golnaz.brj@gmail.com>
2023-07-03 01:31:16 -0400

    added function to fetch five days forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,33 +10,30 @@ var futureTempEl = document.getElementsByClassName("future-temp");
 var futureWindEl = document.getElementsByClassName("future-wind");
 var futureHumidityEl = document.getElementsByClassName("future-humidity");
 
-console.log(futureDateEl);
-
-
-// var requestUrl = 'api.openweathermap.org/data/2.5/forecast?q={city name}&&cnt=1&appid=670f2a326654e8e4ee66af45094f3c93';
-
-// fetch(requestUrl)
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (data) {
-    // console.log(data);
-    // cityNameEl.textContent = `${data.city.name} ${data.list[0].dt_txt}`;
-    // tempEl.textContent = `temp: ${data.list[0].main.temp}`;
-    // humidityEl.textContent = `humidity: ${data.list[0].main.humidity} %`;
-
-  //});
-
 
 
 // Add an event listener to the search button
 searchBtnEl.addEventListener('click', () => {
   const cityName = cityInputEl.value;
 
-  const baseUrl = 'http://api.openweathermap.org/data/2.5/forecast';
-  const updatedUrl = `${baseUrl}?q=${encodeURIComponent(cityName)}&cnt=40&units=metric&appid=670f2a326654e8e4ee66af45094f3c93`;
  
-  console.log(updatedUrl);
+  const todayBaseUrl = 'http://api.openweathermap.org/data/2.5/weather';
+  const todayUpdatedUrl = `${todayBaseUrl}?q=${encodeURIComponent(cityName)}&cnt=1&units=metric&appid=670f2a326654e8e4ee66af45094f3c93`;
+
+  fetch(todayUpdatedUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    cityNameEl.textContent = cityName;
+    console.log(data);
+    tempEl.textContent = `Temp: ${data.main.temp} C`;
+    windEl.textContent = `Wind speed: ${data.wind.speed} MPH`;
+    humidityEl.textContent = `Humidity: ${data.main.humidity} %`;
+  });
+
+  const baseUrl = 'http://api.openweathermap.org/data/2.5/forecast';
+  const updatedUrl = `${baseUrl}?q=${encodeURIComponent(cityName)}&cnt=54&units=metric&appid=670f2a326654e8e4ee66af45094f3c93`;
 
   fetch(updatedUrl)
   .then(function (response) {
@@ -44,16 +41,16 @@ searchBtnEl.addEventListener('click', () => {
   })
   .then(function (data) {
    console.log(data);
-   
+   cityNameEl.text
    console.log(data.list.length);
     j=0;
-   for (var i = 0; i < data.list.length; i+=8) {
+   for (var i = 7; i < data.list.length; i+=8) {
     console.log(data.list[i].dt_txt);
-    
     futureDateEl[j].textContent = data.list[i].dt_txt;
+    futureTempEl[j].textContent = `temp: ${data.list[i].main.temp} C`;
+    futureWindEl[j].textContent = `wind: ${data.list[i].wind.speed} MPH`;
+    futureHumidityEl[j].textContent = `humidity:${data.list[i].main.humidity} %`;
     j++;
-
-  }
-
+   }
   });
 });
